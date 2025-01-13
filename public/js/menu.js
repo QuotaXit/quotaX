deee11972611d6ee3eb6c1d1195e2093c62491f0
quotaxit <silviogavioli@outlook.it>
2025-01-13 04:03:28 +0100

    Messaggio del commit

--- public/js/menu.js
+++ public/js/menu.js
@@ -1,33 +1,10 @@
-function mostraMessaggioDebug(messaggio) {
-    const debugDiv = document.getElementById("debug-messages");
-    if (debugDiv) {
-        const nuovoMessaggio = document.createElement("p");
-        nuovoMessaggio.textContent = messaggio;
-        debugDiv.appendChild(nuovoMessaggio);
-    }
-}
-
 document.addEventListener("DOMContentLoaded", () => {
-    mostraMessaggioDebug("messages.js è stato caricato.");
-    const messageIcons = document.querySelectorAll(".message-icon");
-    mostraMessaggioDebug("Icone trovate: " + messageIcons.length);
+    const hamburger = document.querySelector(".hamburger");
+    const menu = document.querySelector("#menu");
 
-    const modal = document.getElementById("message-modal");
-    const announcementIdInput = document.getElementById("announcementId");
-
-    messageIcons.forEach(icon => {
-        icon.addEventListener("click", () => {
-            mostraMessaggioDebug("Icona cliccata.");
-            const announcementId = icon.getAttribute("data-announcement-id");
-            mostraMessaggioDebug("ID annuncio: " + announcementId);
-
-            if (announcementIdInput && modal) {
-                announcementIdInput.value = announcementId;
-                modal.classList.add("show");
-                mostraMessaggioDebug("La modale è stata mostrata.");
-            } else {
-                mostraMessaggioDebug("Errore: modale o input ID non trovati.");
-            }
+    if (hamburger && menu) {
+        hamburger.addEventListener("click", () => {
+            menu.classList.toggle("hidden");
         });
-    });
+    }
 });
